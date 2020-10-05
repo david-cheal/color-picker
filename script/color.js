f2c7d2b402094eb3d29482d2c9a9cd6781c160aa
David Cheal <dwryan1996@outlook.com>
2020-10-05 08:31:06 +0100

    More value feedback | localstorage for color store

--- script/color.js
+++ script/color.js
@@ -1,9 +1,17 @@
+ var color = "";
+ var storage = window.localStorage;
+
  //When the window loads execute a function
  window.onload = function() {
+
+     //Loads the last color generated
+     color = storage.getItem('color');
+     background.style.backgroundColor = color;
+
      //Wait for any change to sliders and update the color of the background
      $("input").change(function() {
          UpdateColor();
-     })
+     });
  }
 
  function UpdateColor() {
@@ -13,11 +21,25 @@
      var sliderG = document.getElementById("rangeGreen");
      var sliderB = document.getElementById("rangeBlue");
 
+     var colorText = document.getElementById("colorText");
+
+     var valueR = document.getElementById("valueRed");
+     var valueG = document.getElementById("valueGreen");
+     var valueB = document.getElementById("valueBlue");
+
+     valueR.innerHTML = sliderR.value;
+     valueG.innerHTML = sliderG.value;
+     valueB.innerHTML = sliderB.value;
+
      //Gets the background 
      var background = document.getElementById("background");
 
      //Sets the color to be equal to each value from sliders
-     var color = "rgb(" + sliderR.value + ',' + sliderG.value + ',' + sliderB.value + ")";
+     color = "rgb(" + sliderR.value + ',' + sliderG.value + ',' + sliderB.value + ")";
+     colorText.innerHTML = color;
      background.style.backgroundColor = color;
+
+     //Stores the generated color incase of refresh
+     storage.setItem('color', color);
 
  }
